7b6d68c9f30bc9c648ad3bb5ea738a8ce79b71dd
erwincarlogonzales <erwincarlogonzales@gmail.com>
2025-01-15 10:28:15 +0800

    removed test button

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,8 @@
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import MealPlanner from '@/components/MealPlanner'
 
 export default function Home() {
   return (
     <main className="p-4">
-      {/* Test Components */}
-      <Card className="mb-4">
-        <CardHeader>
-          <CardTitle>Test Card</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <Button>Test Button</Button>
-        </CardContent>
-      </Card>
-
-      {/* Main MealPlanner Component */}
       <MealPlanner />
     </main>
   )
